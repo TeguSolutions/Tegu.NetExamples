88ae4c9754f050416b73887c5addcc7396b8df95
Istvan Piroska <istvan@tegusolutions.com>
2022-06-02 12:57:16 +0200

    Client.Maui project added

---
+++ Tegu.Net.Client.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tegu.Net.Client.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Tegu.Net.Client.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tegu.Net.Client.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
