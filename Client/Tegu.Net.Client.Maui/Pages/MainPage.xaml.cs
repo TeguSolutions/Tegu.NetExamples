28894f9fa9016551031caa24dde093640780c651
Istvan Piroska <istvan@tegusolutions.com>
2022-06-13 14:00:18 +0200

    [Maui] Page templates clean up

--- Client/Tegu.Net.Client.Maui/Pages/MainPage.xaml.cs
+++ Client/Tegu.Net.Client.Maui/Pages/MainPage.xaml.cs
@@ -1,26 +1,11 @@
-﻿namespace Tegu.Net.Client.Maui.Pages
+﻿namespace Tegu.Net.Client.Maui.Pages;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage(MainVM vm)
     {
-        int count = 0;
+        InitializeComponent();
 
-        public MainPage(MainVM vm)
-        {
-            InitializeComponent();
-
-            BindingContext = vm;
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        BindingContext = vm;
     }
 }
