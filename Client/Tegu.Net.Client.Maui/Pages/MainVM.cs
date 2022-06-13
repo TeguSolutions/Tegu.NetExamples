1b8bd5c96f7099fd925453b8cf45586faace1bd8
Istvan Piroska <istvan@tegusolutions.com>
2022-06-13 14:25:01 +0200

    [Maui] Shell Navigation

--- Client/Tegu.Net.Client.Maui/Pages/MainVM.cs
+++ Client/Tegu.Net.Client.Maui/Pages/MainVM.cs
@@ -5,5 +5,19 @@ namespace Tegu.Net.Client.Maui.Pages;
 
 public partial class MainVM : ViewModelBase
 {
+    [ICommand]
+    public async void NavigateToControls()
+    {
+        await Shell.Current.GoToAsync(nameof(ControlsPage), true);
+    }
 
+    [ICommand]
+    public async void NavigateToMVVM()
+    {
+        //await Shell.Current.GoToAsync(nameof(MVVMOnePage), true);
+        //await Shell.Current.GoToAsync("MVVM", true);
+
+        //await Shell.Current.GoToAsync($"//{nameof(LoginPage)}", true);
+        await Shell.Current.GoToAsync($"//{nameof(MVVMOnePage)}", true);
+    }
 }
